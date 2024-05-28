aaa503cf6090c58600d6a25a4eaad7932191aef4
Max Lichter <iammaxlichter@gmail.com>
2024-05-28 16:59:39 -0500

    Updates

---
+++ precache-manifest.16a017d98f4c79972d5561154c10d137.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6992c8b6b0377d10d2503eec45bb7864",
+    "url": "/ssg_cookbook2/index.html"
+  },
+  {
+    "revision": "c4f3f59a782026005cee",
+    "url": "/ssg_cookbook2/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "de9404e8eeb3f15186e6",
+    "url": "/ssg_cookbook2/static/css/main.e74092f7.chunk.css"
+  },
+  {
+    "revision": "c4f3f59a782026005cee",
+    "url": "/ssg_cookbook2/static/js/2.b5bfd8b5.chunk.js"
+  },
+  {
+    "revision": "47f7eb342e9ec131679f",
+    "url": "/ssg_cookbook2/static/js/3.0db04885.chunk.js"
+  },
+  {
+    "revision": "de9404e8eeb3f15186e6",
+    "url": "/ssg_cookbook2/static/js/main.d6185315.chunk.js"
+  },
+  {
+    "revision": "347c1e25968e6ecf540e",
+    "url": "/ssg_cookbook2/static/js/runtime~main.717d3064.js"
+  },
+  {
+    "revision": "41b135d9f59c12c06750906b4e086ab4",
+    "url": "/ssg_cookbook2/static/media/Alena.41b135d9.jpg"
+  },
+  {
+    "revision": "b9aa74f2c537e3c549ec08e437bceac1",
+    "url": "/ssg_cookbook2/static/media/Anthony.b9aa74f2.jpg"
+  },
+  {
+    "revision": "459ac291c28b72f66a03bd03d6a11165",
+    "url": "/ssg_cookbook2/static/media/Araceli.459ac291.jpg"
+  },
+  {
+    "revision": "bdb2cc16078dae04e539536e4cfc5b5e",
+    "url": "/ssg_cookbook2/static/media/BethanyA.bdb2cc16.jpg"
+  },
+  {
+    "revision": "fb3042b7bae0e1e0486c1c4ccd538073",
+    "url": "/ssg_cookbook2/static/media/BethanyM.fb3042b7.jpg"
+  },
+  {
+    "revision": "197386982737567cbc2fc17ea8720313",
+    "url": "/ssg_cookbook2/static/media/BethanyQR.19738698.jpg"
+  },
+  {
+    "revision": "5547662b334e52640f372d8c23b1285b",
+    "url": "/ssg_cookbook2/static/media/Brian.5547662b.jpg"
+  },
+  {
+    "revision": "10422f7455453c50bb8609a4a4066e9b",
+    "url": "/ssg_cookbook2/static/media/CBCover.10422f74.png"
+  },
+  {
+    "revision": "79b6f360feb60281e752c27524563f32",
+    "url": "/ssg_cookbook2/static/media/Damber.79b6f360.jpg"
+  },
+  {
+    "revision": "2b55c067b9a6d38ad46629b6edc234ef",
+    "url": "/ssg_cookbook2/static/media/Emily.2b55c067.jpg"
+  },
+  {
+    "revision": "24a77fed671fa4775972116381b387e3",
+    "url": "/ssg_cookbook2/static/media/Eric.24a77fed.jpg"
+  },
+  {
+    "revision": "719fbd91fe3b1b56dba89b90fb352b43",
+    "url": "/ssg_cookbook2/static/media/Kat.719fbd91.jpg"
+  },
+  {
+    "revision": "45f30dc98452124609ca2bf96cde4dd3",
+    "url": "/ssg_cookbook2/static/media/Max.45f30dc9.jpg"
+  },
+  {
+    "revision": "c1e21d1cd84b71959b350c4dbfdf406d",
+    "url": "/ssg_cookbook2/static/media/Michael.c1e21d1c.jpg"
+  },
+  {
+    "revision": "402cb01146d7eeedb767c9c271771c54",
+    "url": "/ssg_cookbook2/static/media/Molly.402cb011.jpg"
+  },
+  {
+    "revision": "fea459e8d426ba1ea50a56899e23b79f",
+    "url": "/ssg_cookbook2/static/media/Momo.fea459e8.jpg"
+  },
+  {
+    "revision": "4600842a8ded761fe9c823263a06e466",
+    "url": "/ssg_cookbook2/static/media/Ojaswi.4600842a.jpg"
+  },
+  {
+    "revision": "e8b76c064845995620bdd1180ced8523",
+    "url": "/ssg_cookbook2/static/media/Rave.e8b76c06.png"
+  },
+  {
+    "revision": "8c648e0ec4e62ae91988545bde5c848b",
+    "url": "/ssg_cookbook2/static/media/Riley.8c648e0e.jpg"
+  },
+  {
+    "revision": "ed2cd98211debc1bbe017906749cb40e",
+    "url": "/ssg_cookbook2/static/media/Savana.ed2cd982.jpg"
+  },
+  {
+    "revision": "93360cdf2164dd3ce18c807d620011d2",
+    "url": "/ssg_cookbook2/static/media/Sophia.93360cdf.jpg"
+  },
+  {
+    "revision": "e6c0df6e3658a0eeee8de0cfa807b36e",
+    "url": "/ssg_cookbook2/static/media/Wontons.e6c0df6e.jpg"
+  },
+  {
+    "revision": "0d0301f9dc7079ddc36411d35917c07b",
+    "url": "/ssg_cookbook2/static/media/Zeeshan.0d0301f9.jpg"
+  },
+  {
+    "revision": "3e51a48f4f05f752f8682a23f0e65aa9",
+    "url": "/ssg_cookbook2/static/media/apple.3e51a48f.jpg"
+  },
+  {
+    "revision": "1bb3c97e40320f6bb69ae8b7f7ccc231",
+    "url": "/ssg_cookbook2/static/media/banana.1bb3c97e.jfif"
+  },
+  {
+    "revision": "23ff75ea668c374a8589dc887100833c",
+    "url": "/ssg_cookbook2/static/media/bawl.23ff75ea.jfif"
+  },
+  {
+    "revision": "f66c31a2f782b9d4d390f5fa299d59fa",
+    "url": "/ssg_cookbook2/static/media/dal.f66c31a2.jpg"
+  },
+  {
+    "revision": "4a81c0d525d56fb29cfd85593fb3ebce",
+    "url": "/ssg_cookbook2/static/media/homeBackground.4a81c0d5.png"
+  },
+  {
+    "revision": "c497b7a4530fc3bdab934d5120a977e8",
+    "url": "/ssg_cookbook2/static/media/jalapeno.c497b7a4.jfif"
+  },
+  {
+    "revision": "4d090e9140a335b77f35350a16afbd40",
+    "url": "/ssg_cookbook2/static/media/paypal.4d090e91.png"
+  },
+  {
+    "revision": "a737c582b67ff9cc4d2833c15c681e02",
+    "url": "/ssg_cookbook2/static/media/ribs.a737c582.jfif"
+  },
+  {
+    "revision": "209604853ad3927e36126ed7970ac695",
+    "url": "/ssg_cookbook2/static/media/ssgCBlogo.20960485.png"
+  },
+  {
+    "revision": "b4c780d6a09b143b5dc0918879747cbd",
+    "url": "/ssg_cookbook2/static/media/taco.b4c780d6.jpg"
+  },
+  {
+    "revision": "a7d34db01a7359a26b26555503ffb8d8",
+    "url": "/ssg_cookbook2/static/media/teamPhoto.a7d34db0.jpg"
+  },
+  {
+    "revision": "49e08b9692e67f870b20902cd0e38368",
+    "url": "/ssg_cookbook2/static/media/zelle.49e08b96.png"
+  }
+]);
